efacfd3187ec3b2b320b55a980aeef1121029f52
Stephen McKamey <mckamey@users.noreply.github.com>
2009-08-19 04:05:06 +0000

    - reworking deltas to reduce complexity of NodeDelta & remove need for CatalogDelta

--- ShadowTracker/Core/Agent/Synchronizer.cs
+++ ShadowTracker/Core/Agent/Synchronizer.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 
 using Shadow.Model;
@@ -10,71 +10,28 @@ namespace Shadow.Agent
 	{
 		#region Delta Methods
 
-		public void SyncCatalogs(Catalog local, Catalog target)
+		public IEnumerable<NodeDelta> GetDelta(Catalog local, Catalog target)
 		{
-			CatalogDelta delta = this.GetDelta(local, target);
-
-			using (TextWriter writer = File.CreateText(@"X:\ExampleActions.txt"))
-			{
-				foreach (NodeDelta action in delta.Actions)
-				{
-					switch(action.Action)
-					{
-						case DeltaAction.Meta:
-						{
-							this.UpdateNode(writer, action);
-							break;
-						}
-						case DeltaAction.Copy:
-						{
-							this.CopyNode(writer, action, action.Local.Path);
-							break;
-						}
-						case DeltaAction.Data:
-						{
-							this.AddNode(writer, action);
-							break;
-						}
-						default:
-						case DeltaAction.None:
-						{
-							writer.WriteLine("ERROR! Unknown action");
-							break;
-						}
-					}
-
-					writer.WriteLine();
-				}
-
-				foreach (string path in delta.Extras)
-				{
-					this.RemoveNode(writer, path);
-				}
-			}
-		}
-
-		public CatalogDelta GetDelta(Catalog local, Catalog target)
-		{
-			CatalogDelta delta = new CatalogDelta();
-
 			// sequence of actions to take place
-			delta.Actions =
-				from node in target.Entries
-				let action = CalcNodeDelta(local, node)
-				where action != null
-				orderby action.Action
-				select action;
-
-			// extras are any local entries not contained in target
-			delta.Extras =
-				from node in local.Entries
-				where !target.ContainsPath(node.Path)
-				select node.Path;
-
-			return delta;
+			return (
+					from node in target.Entries
+					let action = this.CalcNodeDelta(local, node)
+					where action != null
+					orderby action.Action
+					select action
+				).Union(
+					// extras are any local entries not contained in target
+					from node in local.Entries
+					where !target.ContainsPath(node.Path)
+					select new NodeDelta
+						{
+							Action=DeltaAction.Delete,
+							LocalPath=node.Path
+						}
+				);
 		}
 
-		private static NodeDelta CalcNodeDelta(Catalog catalog, DataNode target)
+		private NodeDelta CalcNodeDelta(Catalog catalog, DataNode target)
 		{
 			// look for existing node
 			if (!catalog.ContainsPath(target.Path))
@@ -82,11 +39,11 @@ namespace Shadow.Agent
 				// check if is an empty directory
 				if (target.IsDirectory)
 				{
-					// can build directory from metadata
+					// can build directory from metadata alone
 					return new NodeDelta
 					{
-						Action=DeltaAction.Meta,
-						Target=target
+						Action = DeltaAction.Meta,
+						Target = target
 					};
 				}
 
@@ -96,17 +53,17 @@ namespace Shadow.Agent
 					// equivalent file found
 					return new NodeDelta
 					{
-						Action=DeltaAction.Copy,
-						Local=catalog.GetNodeWithSignature(target.Signature),
-						Target=target
+						Action = DeltaAction.Clone,
+						LocalPath = catalog.GetPathOfNodeWithSignature(target.Signature),
+						Target = target
 					};
 				}
 
-				// completely missing file, need to download
+				// completely missing file, need to add
 				return new NodeDelta
 				{
-					Action=DeltaAction.Data,
-					Target=target
+					Action = DeltaAction.Add,
+					Target = target
 				};
 			}
 
@@ -119,12 +76,11 @@ namespace Shadow.Agent
 
 			if (StringComparer.OrdinalIgnoreCase.Equals(local.Signature, target.Signature))
 			{
-				// file exists with correct bits but metadata has changed
+				// correct bits exist at correct path but metadata is different
 				return new NodeDelta
 				{
-					Action=DeltaAction.Meta,
-					Local=local,
-					Target=target
+					Action = DeltaAction.Meta,
+					Target = target
 				};
 			}
 
@@ -134,49 +90,68 @@ namespace Shadow.Agent
 				// equivalent file found
 				return new NodeDelta
 				{
-					Action=DeltaAction.Copy,
-					Local=catalog.GetNodeWithSignature(target.Signature),
-					Target=target
+					Action = DeltaAction.Clone,
+					LocalPath = catalog.GetPathOfNodeWithSignature(target.Signature),
+					Target = target
 				};
 			}
 
 			// file exists but bits are different
 			return new NodeDelta
 			{
-				Action=DeltaAction.Data,
-				Local=local,
-				Target=target
+				Action = DeltaAction.Update,
+				LocalPath = local.Path,
+				Target = target
 			};
 		}
 
 		#endregion Delta Methods
 
-		#region Events
+		#region Event Placeholders
 
-		private void AddNode(TextWriter writer, NodeDelta action)
+		public void SyncCatalogs(Catalog local, Catalog target)
 		{
-			writer.WriteLine("DOWNLOAD \"{0}\"", action.Target.Signature);
-			string sourcePath = action.Target.Signature;
-			this.CopyNode(writer, action, sourcePath);
+			IEnumerable<NodeDelta> delta = this.GetDelta(local, target);
+
+			foreach (NodeDelta action in delta)
+			{
+				switch (action.Action)
+				{
+					case DeltaAction.Add:
+					{
+						Console.WriteLine("ADD \"{0}\" at \"{1}\"", action.Target.Signature, action.Target.Path);
+						break;
+					}
+					case DeltaAction.Clone:
+					{
+						Console.WriteLine("COPY: \"{0}\" to \"{1}\"", action.LocalPath, action.Target.Path);
+						break;
+					}
+					case DeltaAction.Update:
+					{
+						Console.WriteLine("REPLACE: \"{0}\" to \"{1}\"", action.Target.Signature, action.Target.Path);
+						break;
+					}
+					case DeltaAction.Meta:
+					{
+						Console.WriteLine("ATTRIB: \"{0}\"", action.Target.Path);
+						break;
+					}
+					case DeltaAction.Delete:
+					{
+						Console.WriteLine("REMOVE: \"{0}\"", action.LocalPath);
+						break;
+					}
+					default:
+					case DeltaAction.None:
+					{
+						Console.WriteLine("ERROR: "+action);
+						break;
+					}
+				}
+			}
 		}
 
-		private void CopyNode(TextWriter writer, NodeDelta action, string sourcePath)
-		{
-			writer.WriteLine("COPY: \"{0}\" to \"{1}\"", sourcePath, action.Target.Path);
-			this.UpdateNode(writer, action);
-		}
-
-		private void UpdateNode(TextWriter writer, NodeDelta action)
-		{
-			writer.WriteLine("ATTRIB: \"{0}\"", action.Target.Path);
-		}
-
-		private void RemoveNode(TextWriter writer, string path)
-		{
-			writer.WriteLine("REMOVE: \"{0}\"", path);
-			writer.WriteLine();
-		}
-
-		#endregion Events
+		#endregion Event Placeholders
 	}
 }
