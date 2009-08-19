8437d08bb23dc6048b6beff7cdf69c207e49b56e
smm <smm@a89d3c92-8c60-11de-b760-4d83c623b0ac>
2009-08-19 02:54:04 +0000

    - removing JSON serialization attributes from model

--- trunk/ShadowTracker/Core/Model/DataNode.cs
+++ trunk/ShadowTracker/Core/Model/DataNode.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 
-using JsonFx.Json;
-
 namespace Shadow.Model
 {
 	/// <summary>
@@ -18,26 +16,21 @@ namespace Shadow.Model
 		/// <summary>
 		/// Gets and sets the relative path to the data
 		/// </summary>
-		[JsonName("P")]
 		public string Path { get; set; }
 
 		/// <summary>
 		/// Gets and sets file attributes
 		/// </summary>
-		[JsonName("A")]
 		public FileAttributes Attributes { get; set; }
 
 		/// <summary>
 		/// Gets and sets created date
 		/// </summary>
-		[JsonName("C")]
-		[JsonSpecifiedProperty("HasCreatedDate")]
 		public DateTime CreatedDate { get; set; }
 
 		/// <summary>
 		/// Gets if this node has an associated CreatedDate
 		/// </summary>
-		[JsonIgnore]
 		public bool HasCreatedDate
 		{
 			get { return (this.CreatedDate != default(DateTime)); }
@@ -46,14 +39,11 @@ namespace Shadow.Model
 		/// <summary>
 		/// Gets and sets modified date
 		/// </summary>
-		[JsonName("M")]
-		[JsonSpecifiedProperty("HasModifiedDate")]
 		public DateTime ModifiedDate { get; set; }
 
 		/// <summary>
 		/// Gets if this node has an associated ModifiedDate
 		/// </summary>
-		[JsonIgnore]
 		public bool HasModifiedDate
 		{
 			get { return (this.ModifiedDate != default(DateTime)); }
@@ -62,14 +52,11 @@ namespace Shadow.Model
 		/// <summary>
 		/// Gets and sets the hash signature of the file
 		/// </summary>
-		[JsonName("H")]
-		[JsonSpecifiedProperty("HasSignature")]
 		public string Signature { get; set; }
 
 		/// <summary>
 		/// Gets if this node has an associated hash signature
 		/// </summary>
-		[JsonIgnore]
 		public bool HasSignature
 		{
 			get { return !String.IsNullOrEmpty(this.Signature); }
@@ -78,7 +65,6 @@ namespace Shadow.Model
 		/// <summary>
 		/// Gets if this node represents a directory
 		/// </summary>
-		[JsonIgnore]
 		public bool IsDirectory
 		{
 			get { return ((this.Attributes&FileAttributes.Directory) != 0); }
