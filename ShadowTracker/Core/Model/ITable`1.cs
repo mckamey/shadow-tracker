6dc6465c9c2de3550a9dc2ce9b5d7a880cfccefe
Stephen McKamey <mckamey@users.noreply.github.com>
2009-08-20 04:46:41 +0000

    - adding abstract for persistence - implementing in-memory persistence (for unit testing)

---
+++ ShadowTracker/Core/Model/ITable`1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shadow.Model
+{
+	public interface ITable<T> :
+		IQueryable<T>,
+		IEnumerable<T>
+	{
+		/// <summary>
+		/// Adds an item
+		/// </summary>
+		/// <param name="item"></param>
+		void Add(T item);
+
+		/// <summary>
+		/// Updates an item
+		/// </summary>
+		/// <param name="item"></param>
+		void Update(T item);
+
+		/// <summary>
+		/// Deletes an item
+		/// </summary>
+		/// <param name="item"></param>
+		void Remove(T item);
+
+		/// <summary>
+		/// Deletes a set of items
+		/// </summary>
+		/// <param name="match"></param>
+		void RemoveWhere(Predicate<T> match);
+	}
+}
