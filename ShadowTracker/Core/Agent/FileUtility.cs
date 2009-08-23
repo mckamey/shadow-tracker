f18f32f2f97040a2e2c2bf9797a756694fae4868
Stephen McKamey <mckamey@users.noreply.github.com>
2009-08-23 20:05:02 +0000

    - added trickle update ability

--- ShadowTracker/Core/Agent/FileUtility.cs
+++ ShadowTracker/Core/Agent/FileUtility.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 using Shadow.Model;
 
@@ -23,15 +25,26 @@ namespace Shadow.Agent
 		/// </summary>
 		private const FileAttributes FilteredFiles = FileAttributes.Hidden|FileAttributes.System|FileAttributes.Temporary;
 
+		/// <summary>
+		/// Trickle update rate
+		/// </summary>
+		private const int DefaultTrickleRate = 100;//milliseconds
+
 		#endregion Constants
 
 		#region Methods
+
+		public static void SyncCatalog(Catalog catalog, string rootPath)
+		{
+			FileUtility.SyncCatalog(catalog, rootPath, FileUtility.DefaultTrickleRate);
+		}
 
 		/// <summary>
 		/// Syncs an existing catalog with file system.
 		/// </summary>
 		/// <param name="rootPath"></param>
-		public static void SyncCatalog(Catalog catalog, string rootPath)
+		/// <param name="timerDelay">number of milliseconds to wait between each file processed (for trickle updates)</param>
+		public static void SyncCatalog(Catalog catalog, string rootPath, int trickleRate)
 		{
 			if (catalog == null)
 			{
@@ -44,16 +57,51 @@ namespace Shadow.Agent
 
 			rootPath = rootPath.TrimEnd(Path.DirectorySeparatorChar);
 
-			// TODO: perform this loop with a timer to allow trickle updates
 			var files = FileIterator.GetFiles(rootPath, true).Where(FileUtility.FilterFiles);
-			foreach (FileSystemInfo node in files)
-			{
-				CatalogEntry entry = FileUtility.CreateEntry(rootPath, node);
 
-				catalog.ApplyChanges(entry);
+			if (trickleRate > 0)
+			{
+				var enumerator = files.GetEnumerator();
+
+				// perform loop with a timer to allow trickle updates
+				// use a closure as the callback to allow access to local vars
+				Timer timer = null;
+				timer = new Timer(
+					delegate(object state)
+					{
+						// check if any files left
+						if (!enumerator.MoveNext())
+						{
+							// free closure references
+							timer.Change(Timeout.Infinite, Timeout.Infinite);
+							timer = null;
+							files = null;
+							return;
+						}
+
+						// sync next node
+						CatalogEntry entry = FileUtility.CreateEntry(rootPath, enumerator.Current);
+						catalog.ApplyChanges(entry);
+
+						// queue up next iteration
+						timer.Change(trickleRate, Timeout.Infinite);
+					},
+					null,
+					trickleRate,
+					Timeout.Infinite);
+			}
+			else
+			{
+				foreach (FileSystemInfo file in files)
+				{
+					// sync each node
+					CatalogEntry entry = FileUtility.CreateEntry(rootPath, file);
+					catalog.ApplyChanges(entry);
+				}
 			}
 
-			// TODO: remove any extra files here
+			// TODO: remove any extra files after
+			// TODO: try-catch around work and create a retry queue
 		}
 
 		/// <summary>
