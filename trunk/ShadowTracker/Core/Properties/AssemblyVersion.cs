a41d3bb21347e8e6784531edbda3a82fb31ad8f3
smm <smm@a89d3c92-8c60-11de-b760-4d83c623b0ac>
2009-09-12 16:13:06 +0000

    - rev'ing build number

--- trunk/ShadowTracker/Core/Properties/AssemblyVersion.cs
+++ trunk/ShadowTracker/Core/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.909.1100
+#region 1.0.909.1209
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.909.1100")]
-[assembly: AssemblyFileVersion("1.0.909.1100")]
+[assembly: AssemblyVersion("1.0.909.1209")]
+[assembly: AssemblyFileVersion("1.0.909.1209")]
 
-#endregion 1.0.909.1100
+#endregion 1.0.909.1209
