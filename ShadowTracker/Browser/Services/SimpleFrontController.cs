12b0293ff1a8af2d71cec46cd37f4eb56f72b4be
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-28 03:56:58 +0000

    - apparently completely missed this file

---
+++ ShadowTracker/Browser/Services/SimpleFrontController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.UI;
+
+namespace Shadow.Browser.Services
+{
+	/// <summary>
+	/// Loads the view which corresponds to the URL.
+	/// </summary>
+	public class SimpleFrontController : IHttpHandlerFactory
+	{
+		#region Constants
+
+		private const string DefaultFile = "Default.aspx";
+		private const string ViewPath = "~/"+DefaultFile;
+
+		#endregion Constants
+
+		#region IHttpHandlerFactory Members
+
+		IHttpHandler IHttpHandlerFactory.GetHandler(HttpContext context, string requestType, string virtualPath, string physicalPath)
+		{
+			if (virtualPath.EndsWith(DefaultFile, StringComparison.OrdinalIgnoreCase))
+			{
+				virtualPath = virtualPath.Substring(0, virtualPath.Length-DefaultFile.Length);
+				context.RewritePath(virtualPath, true);
+			}
+			else if (!virtualPath.EndsWith("/", StringComparison.OrdinalIgnoreCase))
+			{
+				context.Response.Redirect(virtualPath+'/');
+			}
+
+			if (String.IsNullOrEmpty(context.Request.QueryString[null]))
+			{
+				context.Items["TreeView"] = "UIT.ExpandoTree";
+			}
+			else
+			{
+				context.Items["TreeView"] = "UIT.FileFolderTree";
+			}
+
+			return BuildManager.CreateInstanceFromVirtualPath(ViewPath, typeof(Page)) as Page;
+		}
+
+		void IHttpHandlerFactory.ReleaseHandler(IHttpHandler handler)
+		{
+		}
+
+		#endregion IHttpHandlerFactory Members
+	}
+}
