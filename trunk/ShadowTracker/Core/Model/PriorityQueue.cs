29991109ecfe3c0b7bee138d8b4d75ee8506417b
smm <smm@a89d3c92-8c60-11de-b760-4d83c623b0ac>
2009-12-03 15:02:53 +0000

    - adding a very efficient implementation of PriorityQueue and unit test - this will serve as basis for global work queue

---
+++ trunk/ShadowTracker/Core/Model/PriorityQueue.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Shadow.Model
+{
+	/// <summary>
+	/// A queue which always returns the highest priority element.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class PriorityQueue<T> :
+		IEnumerable<T>,
+		ICollection,
+		IEnumerable
+	{
+		#region Constants
+
+		private const int MinCapacity = 4;
+		private const int GrowthRate = 2;
+		private const double TrimThreshold = 0.9;
+
+		#endregion Constants
+
+		#region Fields
+
+		private T[] data;
+		private int count;
+		private int version;
+
+		private readonly Func<T, T, bool> HigherPriority;
+
+		#endregion Fields
+
+		#region Init
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		public PriorityQueue()
+			: this(MinCapacity)
+		{
+		}
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="capacity">initial capacity</param>
+		public PriorityQueue(int capacity)
+			: this(capacity, delegate(T a, T b) { return Comparer<T>.Default.Compare(a, b) == 1; })
+		{
+		}
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="higherPriorityPredicate">predicate that returns true if the first argument is higher priority than the second; if equal or lesser priority returns false</param>
+		public PriorityQueue(Func<T, T, bool> higherPriorityPredicate)
+			: this(MinCapacity, higherPriorityPredicate)
+		{
+		}
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="capacity">initial capacity</param>
+		/// <param name="higherPriorityPredicate">predicate that returns true if the first argument is higher priority than the second; if equal or lesser priority returns false</param>
+		public PriorityQueue(int capacity, Func<T, T, bool> higherPriorityPredicate)
+		{
+			this.HigherPriority = higherPriorityPredicate;
+			this.SetCapacity(capacity);
+		}
+
+		#endregion Init
+
+		#region Queue Methods
+
+		/// <summary>
+		/// Adds an element to the end of the queue and lets it trickle up to the correct position
+		/// </summary>
+		/// <param name="value"></param>
+		public void Enqueue(T value)
+		{
+			// ensure there is enough room to add more
+			if (this.count >= this.data.Length)
+			{
+				// grow internal storage
+				this.SetCapacity(GrowthRate * this.count);
+			}
+
+			// insert value at end of queue
+			int index = this.count;
+			this.data[index] = value;
+			this.count++;
+
+			// trickle up to correct position
+			int parent = (index - 1) / 2;
+			while (index > 0 && this.HigherPriority(this.data[index], this.data[parent]))
+			{
+				this.Swap(index, parent);
+				index = parent;
+				parent = (index - 1) / 2;
+			}
+
+			this.version++;
+		}
+
+		/// <summary>
+		/// Returns the element with the highest priority in the queue without removing it
+		/// </summary>
+		public T Peek()
+		{
+			if (this.count <= 0)
+			{
+				throw new InvalidOperationException("queue is empty");
+			}
+
+			return this.data[0];
+		}
+
+		/// <summary>
+		/// Removes and returns the element with the highest priority in the queue
+		/// </summary>
+		/// <returns></returns>
+		public T Dequeue()
+		{
+			if (this.count <= 0)
+			{
+				throw new InvalidOperationException("queue is empty");
+			}
+
+			T current = this.data[0];
+
+			// move the last item into root and trickle down to correct position
+			this.count--;
+			this.data[0] = this.data[this.count];
+			this.RebuildHeap();
+			this.version++;
+
+			return current;
+		}
+
+		/// <summary>
+		/// Removes all elements from the queue
+		/// </summary>
+		public void Clear()
+		{
+			this.count = 0;
+			this.version++;
+		}
+
+		/// <summary>
+		/// Sets the capacity to the actual number of elements in the queue,
+		/// if that number is less than 90 percent of current capacity
+		/// </summary>
+		public void TrimExcess()
+		{
+			int threshold = (int)(this.data.Length * TrimThreshold);
+			if (this.count < threshold)
+			{
+				this.SetCapacity(this.count);
+			}
+		}
+
+		/// <summary>
+		/// Sets the capacity of the underlying storage
+		/// </summary>
+		/// <param name="capacity"></param>
+		private void SetCapacity(int capacity)
+		{
+			// bound capacity between MinCapacity and the current count
+			capacity = Math.Max(this.count, Math.Max(MinCapacity, capacity));
+
+			T[] array = new T[capacity];
+			if (this.count > 0)
+			{
+				Array.Copy(this.data, 0, array, 0, this.count);
+			}
+			this.data = array;
+		}
+
+		#endregion Queue Methods
+
+		#region Rebuild Heap Methods
+
+		/// <summary>
+		/// Ripples the value at the given index down to the correct position
+		/// </summary>
+		private void RebuildHeap()
+		{
+			// this could be passed in as a parameter for arbitrary rebuilding
+			int index = 0;
+
+			while (this.count > index)
+			{
+				// calc the indices of the children for the given parent index
+				int left = 2 * index + 1;
+				int right = left + 1;
+
+				// find the highest priority between left and right children
+				int priority;
+				if (right >= this.count)
+				{
+					if (left >= this.count)
+					{
+						// index is leaf node, no more trickle down
+						break;
+					}
+
+					// no right node, must be left
+					priority = left;
+				}
+				else
+				{
+					if (this.HigherPriority(this.data[left], this.data[right]))
+					{
+						priority = left;
+					}
+					else
+					{
+						priority = right;
+					}
+				}
+
+				if (this.HigherPriority(this.data[index], this.data[priority]))
+				{
+					// index is already the highest priority of the three nodes
+					break;
+				}
+
+				// highest priority child is higher than index, swap and continue
+				this.Swap(index, priority);
+				index = priority;
+			}
+		}
+
+		/// <summary>
+		/// Swaps the values at the two indices.
+		/// </summary>
+		/// <param name="a">index a</param>
+		/// <param name="b">index b</param>
+		private void Swap(int a, int b)
+		{
+			T temp = this.data[b];
+			this.data[b] = this.data[a];
+			this.data[a] = temp;
+		}
+
+		#endregion Rebuild Heap Methods
+
+		#region ICollection Members
+
+		/// <summary>
+		/// Copies the elements to an existing one-dimensional System.Array, starting at the specified array index.
+		/// </summary>
+		/// <param name="array"></param>
+		/// <param name="index"></param>
+		public void CopyTo(Array array, int index)
+		{
+			// TODO: ensure that this is returned in priority order
+			this.data.CopyTo(array, index);
+		}
+
+		/// <summary>
+		/// Gets the number of elements contained in the queue.
+		/// </summary>
+		public int Count
+		{
+			get { return this.count; }
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether access to the queue is synchronized (thread safe). 
+		/// </summary>
+		public bool IsSynchronized
+		{
+			get { return this.data.IsSynchronized; }
+		}
+
+		/// <summary>
+		/// Gets an object that can be used to synchronize access to the queue.
+		/// </summary>
+		public object SyncRoot
+		{
+			get { return this.data.SyncRoot; }
+		}
+
+		#endregion ICollection Members
+
+		#region IEnumerable<T> Members
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			// TODO: ensure that this is returned in priority order
+			foreach (T item in this.data)
+			{
+				yield return item;
+			}
+		}
+
+		#endregion IEnumerable<T> Members
+
+		#region IEnumerable Members
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			// TODO: ensure that this is returned in priority order
+			return this.data.GetEnumerator();
+		}
+
+		#endregion IEnumerable Members
+	}
+}
---
+++ trunk/ShadowTracker/Core_Test/Model/PriorityQueueTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shadow.Model.Test
+{
+	/// <summary>
+	/// Summary description for PriorityQueueTest
+	/// </summary>
+	[TestClass]
+	public class PriorityQueueTest
+	{
+		private PriorityQueue<int> queue;
+		private Func<int, int, bool> comparer;
+
+		[TestInitialize()]
+		public void InitQueue()
+		{
+			this.comparer = (a, b) => a > b;
+			this.queue = new PriorityQueue<int>(this.comparer);
+		}
+
+		[TestMethod]
+		public void TestRandom()
+		{
+			int[] values = { 0, 5, 4, 3, 8, 7, 2, 1, 0, 9, 8, 1, 5, 6 };
+
+			foreach (int value in values)
+			{
+				this.queue.Enqueue(value);
+			}
+
+			foreach (int value in values.OrderByDescending(n => n))
+			{
+				Assert.AreEqual(value, this.queue.Dequeue());
+			}
+		}
+
+		[TestMethod]
+		public void TestReverseSorted()
+		{
+			int[] values = { 9, 9, 8, 8, 7, 6, 5, 4, 3, 3, 2, 1, 0, 0 };
+
+			foreach (int value in values)
+			{
+				this.queue.Enqueue(value);
+			}
+
+			foreach (int value in values.OrderByDescending(n => n))
+			{
+				Assert.AreEqual(value, this.queue.Dequeue());
+			}
+		}
+
+		[TestMethod]
+		public void TestPreSorted()
+		{
+			int[] values = { 0, 0, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 8, 9, 9 };
+
+			foreach (int value in values)
+			{
+				this.queue.Enqueue(value);
+			}
+
+			foreach (int value in values.OrderByDescending(n => n))
+			{
+				Assert.AreEqual(value, this.queue.Dequeue());
+			}
+		}
+	}
+}
