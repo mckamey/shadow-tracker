5a5c3d7811f928ccdd17801d0cce9abefd462a79
smm <smm@a89d3c92-8c60-11de-b760-4d83c623b0ac>
2009-12-13 19:37:02 +0000

    - v1.0.0912.1311

--- trunk/ShadowTracker/Service/Properties/AssemblyVersion.cs
+++ trunk/ShadowTracker/Service/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.912.516
+#region 1.0.912.1311
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.912.516")]
-[assembly: AssemblyFileVersion("1.0.912.516")]
+[assembly: AssemblyVersion("1.0.912.1311")]
+[assembly: AssemblyFileVersion("1.0.912.1311")]
 
-#endregion 1.0.912.516
+#endregion 1.0.912.1311
