fe91320cccbc299007b819b1222ae17fd73f2504
smm <smm@a89d3c92-8c60-11de-b760-4d83c623b0ac>
2010-02-23 16:42:10 +0000

    - moving PAUSEs to batch files so un/installation can be performed from automated builds

--- trunk/ShadowTracker/Service/Install.bat
+++ trunk/ShadowTracker/Service/Install.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
 :: -------------------------------------------------------------------
 
+ECHO WARNING: are you sure to install ShadowTracker.Service.exe as a Windows Service?
+PAUSE
+ECHO.
+
 ShadowTracker.Service.exe /install
+PAUSE
